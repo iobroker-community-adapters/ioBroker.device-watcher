80f38d49aafe30c2e9ea5e3bec34bdcf8644242b
Christian Behrends <mail@christian-behrends.de>
2023-02-16 23:53:14 +0100

    commented out

--- main.js
+++ main.js
@@ -778,7 +778,7 @@ class DeviceWatcher extends utils.Adapter {
 				const instanceDeviceConnectionDP = `${instance}.info.connection`;
 				const instancedeviceConnected = await this.getInitValue(instanceDeviceConnectionDP);
 				this.subscribeForeignStates(instanceDeviceConnectionDP);
-				this.subscribeForeignObjects(`${this.selAdapter[i].Selektor}`);
+				//this.subscribeForeignObjects(`${this.selAdapter[i].Selektor}`);
 
 				/*=============================================
 				=              Get device name		          =
@@ -797,7 +797,7 @@ class DeviceWatcher extends utils.Adapter {
 				const shortCurrDeviceString = currDeviceString.slice(0, currDeviceString.lastIndexOf('.') + 1 - 1);
 
 				// subscribe to object device path
-				this.subscribeForeignObjects(currDeviceString);
+				//this.subscribeForeignObjects(currDeviceString);
 
 				/*=============================================
 				=            Get signal strength              =
@@ -1930,7 +1930,7 @@ class DeviceWatcher extends utils.Adapter {
 				this.subscribeForeignStates(id);
 				this.subscribeForeignStates(instanceConnectedHostDP);
 				this.subscribeForeignStates(instanceConnectedDeviceDP);
-				this.subscribeForeignObjects(instanceObjectPath);
+				//this.subscribeForeignObjects(instanceObjectPath);
 
 				// create raw list
 				this.listInstanceRaw.set(instanceObjectPath, {
